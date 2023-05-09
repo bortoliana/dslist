3be62159dee052278e581d02eb568ae630c86c70
ana <analeticiadebortoli@gmail.com>
2023-05-08 23:47:31 -0300

    Properties, game, orm

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -25,11 +25,13 @@ public class Game {
     private String title;
     @Column(name = "game_year")
     private Integer year;
-    private String gnre;
+    private String genre;
     private String platforms;
     private Double score;
     private String imgUrl;
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     @Override
